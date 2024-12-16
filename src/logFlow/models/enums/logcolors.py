a4334230e39a6a9a3c78f21884a10d39138d6165
Aleko Khomasuridze <83498669+aleko-khomasuridze@users.noreply.github.com>
2024-12-17 01:02:01 +0400

    .

---
+++ src/logFlow/models/enums/logcolors.py
@@ -0,0 +1,10 @@
+from colorama import Fore, Style
+
+class LogColors:
+    ERROR = Fore.LIGHTRED_EX
+    INFO = Fore.LIGHTBLUE_EX
+    SUCCESS = Fore.LIGHTGREEN_EX
+    WARNING = Fore.LIGHTYELLOW_EX
+    FATAL = Fore.LIGHTMAGENTA_EX
+    RESET = Fore.RESET
+    
---
+++ tests/test_colors.py
@@ -0,0 +1,7 @@
+from colorama import *
+
+print(f'{Back.LIGHTRED_EX}{Fore.LIGHTWHITE_EX}[Error]~[12:23:34-12.32.42]{Fore.RESET}{Back.RESET}: lorem ipsum amet dolor elit')
+print(f'{Back.LIGHTBLUE_EX}{Fore.LIGHTWHITE_EX}[Info]~[12:23:34-12.32.42]{Fore.RESET}{Back.RESET}: lorem ipsum amet dolor elit')
+print(f'{Back.LIGHTGREEN_EX}{Fore.LIGHTWHITE_EX}[Success]~[12:23:34-12.32.42]{Fore.RESET}{Back.RESET}: lorem ipsum amet dolor elit')
+print(f'{Back.YELLOW}{Fore.LIGHTWHITE_EX}[Warning]~[12:23:34-12.32.42]{Fore.RESET}{Back.RESET}: lorem ipsum amet dolor elit')
+print(f'{Back.LIGHTMAGENTA_EX}{Fore.LIGHTWHITE_EX}[Fatal]~[12:23:34-12.32.42]{Fore.RESET}{Back.RESET}: lorem ipsum amet dolor elit')

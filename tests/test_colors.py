b4de0a3cfdeef43fe636dc60ba502cecfef1a3d0
Aleko Khomasuridze <83498669+aleko-khomasuridze@users.noreply.github.com>
2024-12-19 15:35:59 +0400

    Update test_colors.py

--- tests/test_colors.py
+++ tests/test_colors.py
@@ -1,7 +1,7 @@
 from colorama import *
 
-print(f'{Back.LIGHTRED_EX}{Fore.LIGHTWHITE_EX}[Error]~[12:23:34-12.32.42]{Fore.RESET}{Back.RESET}: lorem ipsum amet dolor elit')
-print(f'{Back.LIGHTBLUE_EX}{Fore.LIGHTWHITE_EX}[Info]~[12:23:34-12.32.42]{Fore.RESET}{Back.RESET}: lorem ipsum amet dolor elit')
-print(f'{Back.LIGHTGREEN_EX}{Fore.LIGHTWHITE_EX}[Success]~[12:23:34-12.32.42]{Fore.RESET}{Back.RESET}: lorem ipsum amet dolor elit')
-print(f'{Back.YELLOW}{Fore.LIGHTWHITE_EX}[Warning]~[12:23:34-12.32.42]{Fore.RESET}{Back.RESET}: lorem ipsum amet dolor elit')
-print(f'{Back.LIGHTMAGENTA_EX}{Fore.LIGHTWHITE_EX}[Fatal]~[12:23:34-12.32.42]{Fore.RESET}{Back.RESET}: lorem ipsum amet dolor elit')
+print(f'{Fore.LIGHTRED_EX}[Error]-[12:23:34-12.32.42]{Fore.RESET}{Fore.RESET}: lorem ipsum amet dolor elit')
+print(f'{Fore.LIGHTBLUE_EX}[Info]-[12:23:34-12.32.42]{Fore.RESET}{Fore.RESET}: lorem ipsum amet dolor elit')
+print(f'{Fore.LIGHTGREEN_EX}[Success]-[12:23:34-12.32.42]{Fore.RESET}{Fore.RESET}: lorem ipsum amet dolor elit')
+print(f'{Fore.YELLOW}[Warning]-[12:23:34-12.32.42]{Fore.RESET}{Fore.RESET}: lorem ipsum amet dolor elit')
+print(f'{Fore.LIGHTMAGENTA_EX}[Fatal]-[12:23:34-12.32.42]{Fore.RESET}{Fore.RESET}: lorem ipsum amet dolor elit')
